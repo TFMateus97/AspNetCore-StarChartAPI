563fde0155a1fb14917738bb56f774b8faa2e09f
Mateus Torres <34038838+TFMateus97@users.noreply.github.com>
2022-10-05 22:39:15 -0300

    Implementação das actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,39 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(e => e.Id == id);
+            if (celestialObject == null)
+                return NotFound();
+
+            var orbitedObject = _context.CelestialObjects.FirstOrDefault(e => e.Id == celestialObject.OrbitedObjectId);
+            celestialObject.Satellites.Add(orbitedObject);
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObject = _context.CelestialObjects.FirstOrDefault(e => e.Name == name);
+            if (celestialObject == null)
+                return NotFound();
+
+            var orbitedObject = _context.CelestialObjects.FirstOrDefault(e => e.Id == celestialObject.OrbitedObjectId);
+            celestialObject.Satellites.Add(orbitedObject);
+
+            return Ok(celestialObject);
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+            return Ok(celestialObjects);
+        }
     }
 }
